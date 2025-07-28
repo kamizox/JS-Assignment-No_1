24263d3383f69d2b499488c2ebdd3721e655d309
Kamran Alam <Kamrankhan0368@gmail.com>
2025-07-29 01:14:32 +0500

    Completed form Ch_2

---
+++ CHAPTER_2/Script.js
@@ -0,0 +1,22 @@
+var username;
+var myName = "Kamran Alam";
+var message = "Hello World";
+alert(message);
+var studentName = "Jhone Doe";
+var age = "15 years old";
+var certificate = "Certified Mobile Application Development";
+alert(studentName);
+alert(age);
+alert(certificate);
+var pizza = "PIZZA\nPIZZ\nPIZ\nP";
+alert(pizza)
+
+var email = "Kamrankhan0368@gmail.com";
+alert("My email address is " + email);
+
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn the Book "+ book)
+
+var myString = "\n ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+        alert(myString);
+        document.write(myString);
