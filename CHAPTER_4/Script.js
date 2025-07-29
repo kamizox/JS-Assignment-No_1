10c4a826c1ada43462ea4b1ec5d92f7fbe41e44d
Kamran Alam <Kamrankhan0368@gmail.com>
2025-07-29 16:51:40 +0500

    Ch_4 Completed

---
+++ CHAPTER_4/Script.js
@@ -0,0 +1,16 @@
+var variable_1, variable_2, variable_3;
+
+// legal variable
+var num;
+var num1
+var _num;
+var $num;
+var numOne;
+
+// Illegal variable
+var var;
+var 1num;
+var %num;
+var break;
+var num-1;
+
