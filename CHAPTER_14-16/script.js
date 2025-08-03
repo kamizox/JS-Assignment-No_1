9d3165746b3a623b1705bc29d443f70f9886e87c
Kamran Alam <Kamrankhan0368@gmail.com>
2025-08-03 05:35:31 +0500

    added

---
+++ CHAPTER_14-16/script.js
@@ -0,0 +1,14 @@
+
+var studentsName = [];
+studentsName[0] = "Ali";
+studentsName[1] = "Raza";
+studentsName[2] = "Kaif";
+studentsName[3] = "Khalil";
+
+var studenArr = new Array();
+
+var String = ["Ali","Amjad"]
+var num = [1,3,4,4];
+var bolien = [true];
+var mixArr = [4,5,"alli", true, 0];
+
